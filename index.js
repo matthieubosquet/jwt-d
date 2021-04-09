76d4c022f4cd2760195ca11361ec7b32473fcffd
Matthieu Bosquet <matthieubosquet@gmail.com>
2021-04-09 09:09:50 +0100

    Just copy paste from the browser

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 const [ ,, ...args ] = process.argv;
 
-console.log(JSON.parse(
-  `{
-    "header": ${Buffer.from(args[0].split('.')[0], 'base64').toString()},
-    "payload": ${Buffer.from(args[0].split('.')[1], 'base64').toString()},
-    "signature": "${args[0].split('.')[2]}"
-  }`));
+function display(jwt) {
+  if (!!jwt) {
+    return "Not a JWT";
+  }
+
+  return JSON.parse(
+    `{
+      "header": ${Buffer.from(jwt.split('.')[0], 'base64').toString()},
+      "payload": ${Buffer.from(jwt.split('.')[1], 'base64').toString()},
+      "signature": "${jwt.split('.')[2]}"
+    }`);
+}
+
+const jwt = /:$/.test(args[0]) ? args[1] : args[0];
+
+console.log(display(jwt));
