bfea14f9b3dfd2c8ef8365ef59bdd6b99945137e
Matthieu Bosquet <matthieubosquet@gmail.com>
2021-04-09 09:18:09 +0100

    Fixup browser copy support

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const [ ,, ...args ] = process.argv;
 
 function display(jwt) {
   if (!!jwt) {
-    return "Not a JWT";
+    return JSON.parse(
+      `{
+        "header": ${Buffer.from(jwt.split('.')[0], 'base64').toString()},
+        "payload": ${Buffer.from(jwt.split('.')[1], 'base64').toString()},
+        "signature": "${jwt.split('.')[2]}"
+      }`);
   }
 
-  return JSON.parse(
-    `{
-      "header": ${Buffer.from(jwt.split('.')[0], 'base64').toString()},
-      "payload": ${Buffer.from(jwt.split('.')[1], 'base64').toString()},
-      "signature": "${jwt.split('.')[2]}"
-    }`);
+  return "Not a JWT";
 }
 
 const jwt = /:$/.test(args[0]) ? args[1] : args[0];
